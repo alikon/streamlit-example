bc6607913ccda7ab8be52f2ec8950195f5d5e8d2
Nicola Galgano <optimus4joomla@gmail.com>
2023-03-04 09:42:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,10 @@ import streamlit as st
 # data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
 response = requests.get("http://api.open-notify.org/astros.json")
 st.write(response)
-
+raw = response.content() # Return the raw bytes of the data payload
+st.write(raw)
+text = response.text() # Return a string representation of the data payload
+st.write(text)
+json = response.json() # This method is convenient when the API returns JSON
+st.write(json)
 # st.write(data)
