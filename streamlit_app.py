48f0e3532928a468dbf4bfa5022af7afb8fa4c14
alikon <optimus4joomla@gmail.com>
2023-03-04 11:27:20 +0100

    series

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,6 @@ st.write(columnVersion)
 chart_data = pd.DataFrame(
     valueVersion
     )
-chart_data.columns=columnVersion
+chart_data.columns=pd.Series(columnVersion)
 
 st.line_chart(chart_data)
