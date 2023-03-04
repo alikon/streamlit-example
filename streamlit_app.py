6f841935c3061c4d28b32c0ef94fa6aed30d155a
alikon <optimus4joomla@gmail.com>
2023-03-04 10:47:30 +0100

    graph

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,16 @@ st.json(json['data']['cms_version'])
 versions = json['data']['cms_version']
 for version in versions:
     st.write(version)
-
+columnVersion = []
+valueVersion = []
 for key, val in versions.items():
     st.write(key)
+    columnVersion.append(key)
     st.write(val)
+    valueVersion.append(val)
+
+chart_data = pd.DataFrame(
+    valueVersion,
+    columns=columnVersion)
+
+st.line_chart(chart_data)
