0c42db13e241e6cc7bab2573284f5836dbb6c281
alikon <optimus4joomla@gmail.com>
2023-03-04 10:04:37 +0100

    + panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import requests
 import streamlit as st
 
+import numpy as np
+import pandas as pd
+
+
+st.title("Simple Streamlit App")
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(
+    pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+)
+
 # data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
 response = requests.get("https://developer.joomla.org/stats/cms_version")
 st.write(response)
