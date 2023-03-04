2e87a2cb0991883c22374e749a32cb7e5958b409
alikon <optimus4joomla@gmail.com>
2023-03-04 09:56:44 +0100

    + cms_verion

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,7 @@ import streamlit as st
 # data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
 response = requests.get("https://developer.joomla.org/stats/cms_version")
 st.write(response)
-# raw = response.content() # Return the raw bytes of the data payload
-# st.write(raw)
-# text = response.text() # Return a string representation of the data payload
-# st.write(text)
+
 json = response.json() # This method is convenient when the API returns JSON
 st.write(json)
-# st.write(data)
+st.write(json.data)
