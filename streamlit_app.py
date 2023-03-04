64fb8269602a4a283711cf76e8d7caa6a8ec3360
alikon <optimus4joomla@gmail.com>
2023-03-04 10:26:42 +0100

    version

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,7 @@ st.write(response)
 json = response.json() # This method is convenient when the API returns JSON
 st.write(json)
 st.json(json['data']['cms_version'])
+
+versions = json['data']['cms_version']
+for version in versions:
+    st.write(version)
