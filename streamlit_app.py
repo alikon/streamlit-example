f9844ec672bb55ef554e3502e94a42698cb5cbba
Nicola Galgano <optimus4joomla@gmail.com>
2023-03-04 09:37:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,7 @@ import requests
 import streamlit as st
 
 data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
+response = requests.get("http://api.open-notify.org/astros.json")
+st.write(response)
 
 st.write(data)
